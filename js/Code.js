692fe8d447d14de8ff22cde2202773041d0fe9b9
Smaira14 <74294055+Smaira14@users.noreply.github.com>
2021-08-25 17:36:19 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "GOOGLE";
+const accessCode2 = "HEART";
+const accessCode3 = "CORONA";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("OGGOEL", 100,50)
+    fill("lightblue")
+    text("Hint: Answer to every problem but needs a connection !!", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("EAHTR", 700,150)
+    fill("lightblue")
+    text("Hint: Reason you are alive, never tired of its job !!", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("ORCOAN", 100,250)
+    fill("lightblue")
+    text("Hint: A headache to world afraid of needles !!", 100,270)
+
+}
